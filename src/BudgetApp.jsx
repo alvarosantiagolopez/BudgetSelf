bb92e0a14441a69bcfddcd87134fd68580a97a71
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-08-06 15:38:11 +0200

    first commit

---
+++ src/BudgetApp.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+export const BudgetApp = () => (
+    <div className="app">
+        <header className="header">
+            <h1>BudgetSelf</h1>
+        </header>
+    </div>
+);
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { BudgetApp } from './BudgetApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BudgetApp />
+  </React.StrictMode>
+)
